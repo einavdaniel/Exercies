00ddf3f591eeff605d084f8210976c8ce53214c6
einavdaniel <einav.d911@gmail.com>
2018-02-04 15:57:36 +0200

    start

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,22 @@
 var einavCtrls = angular.module('einavCtrls', ['einavServices']);
 
 einavCtrls.controller('homeCtrl', function ($scope, myService) {
-    $scope.data = myService.myData;
-    $scope.loading = myService.loading;
+    // $scope.data = myService.myData;
+    // // $scope.loading = myService.loading;
+
+    // if (typeof jQuery != 'undefined') {
+
+    //     alert("jQuery library is loaded!");
+    
+    // }else{
+    
+    //     alert("jQuery library is not found!");
+    
+    // }
+
+    // if(typeof($.fn.popover) != 'undefined'){
+    //     alert("bootstrap library is loaded!");
+    //    }
 });
 
 einavCtrls.controller('einavCtrl', function ($scope) {
