7d36f14e30438c1c19b2cb42e4f8df31387673da
einavdaniel <einav.d911@gmail.com>
2019-05-24 17:28:04 +0300

    baseline

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,4 @@
-var einavCtrls = angular.module('einavCtrls', ['einavServices']);
+var einavCtrls = angular.module('einavCtrls', ['einavServices', 'ui.select', 'ngSanitize']);
 
 einavCtrls.controller('homeCtrl', function ($scope, myService) {
     // $scope.data = myService.myData;
@@ -7,16 +7,67 @@ einavCtrls.controller('homeCtrl', function ($scope, myService) {
     // if (typeof jQuery != 'undefined') {
 
     //     alert("jQuery library is loaded!");
-    
+
     // }else{
-    
+
     //     alert("jQuery library is not found!");
-    
+
     // }
 
     // if(typeof($.fn.popover) != 'undefined'){
     //     alert("bootstrap library is loaded!");
     //    }
+    $scope.selected = {
+        location: 'eilat'
+      };
+
+    $scope.$watch('selected.location', function(newVal, oldVal) {
+        if (newVal !== oldVal) {
+          if ($scope.locations.indexOf(newVal) === -1) {
+            $scope.locations.unshift(newVal);
+          }
+        }
+      });
+      $scope.$watch('selected.item', function(newVal, oldVal) {
+        if (newVal !== oldVal) {
+          if ($scope.itemArray.indexOf(newVal) === -1) {
+            //$scope.itemArray.unshift(newVal);
+          }
+          var newvalholder = newVal
+          $scope.itemArray.forEach(item => {
+              if(item.name === newvalholder){
+                  $scope.selected.item = item;
+              }
+          });
+        }
+      });
+
+      $scope.popup = function(item, model){
+          console.log(item , model)
+          console.log(item )
+          console.log( model)
+      }
+    $scope.test = "controller is working";
+
+    $scope.locations = ["eilat", "tel aviv", "haifa"]
+
+    $scope.itemArray = [
+        { id: 1, name: 'first' },
+        { id: 2, name: 'second' },
+        { id: 3, name: 'third' },
+        { id: 4, name: 'fourth' },
+        { id: 5, name: 'fifth' },
+    ];
+
+  //  $scope.area.selected = $scope.locations[0];
+
+    $scope.getLocations = function (search) {
+        var newLocs = $scope.locations.slice();
+        if (search && newLocs.indexOf(search) === -1) {
+            newLocs.unshift(search);
+        }
+        return newLocs;
+    }
 });
 
 einavCtrls.controller('einavCtrl', function ($scope) {
